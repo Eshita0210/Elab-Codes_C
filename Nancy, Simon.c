51d09d0d46d7eb0c5bbcfe1eefbd9638513422b4
Eshita0210 <86978758+Eshita0210@users.noreply.github.com>
2023-03-21 13:25:52 +0530

    Create Nancy, Simon.c

---
+++ Nancy, Simon.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void getFibonacii(int a,int b,int n)
+{int c=0;
+    if(n>0){
+        c=a+b;
+        a=b;
+        b=c;
+        printf("%d ",c);
+        getFibonacii(a,b,n-1);
+    }
+}
+int main()
+{int a=0,b=1,n,x;
+scanf("%d",&x); n=x-1;
+printf("%d %d ",a,b);
+getFibonacii(a,b,n-1);
+               return 0;
+}
